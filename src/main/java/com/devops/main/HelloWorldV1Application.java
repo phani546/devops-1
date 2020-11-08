d28f0ff5360a25cb7a15e781a2102d6edbd227c2
pnadiminti <phani.nadiminti@ktree.us>
2020-11-08 15:08:19 +0000

    initial commit

---
+++ src/main/java/com/devops/main/HelloWorldV1Application.java
@@ -0,0 +1,12 @@
+package com.devops.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldV1Application.class, args);
+	}
+}
